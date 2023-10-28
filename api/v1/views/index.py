544bfa4ffd906d9a56b8c5b90432181afbdab05f
MahmoudEasa <MahmoudEasa@gmail.com>
2023-10-28 21:41:26 +0000

    Fix api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,17 +11,17 @@ def status():
     return jsonify(status="OK")
 
 
-@app_views.route("/stats", strict_slashes=False)
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
 def stats():
     """ Retrieves the number of each objects by type """
-    return (jsonify(
+    return (make_response(jsonify(
             amenities=storage.count('Amenities'),
             cities=storage.count('Cities'),
             places=storage.count('Places'),
             reviews=storage.count('Reviews'),
             states=storage.count('States'),
             users=storage.count('Users')
-            ))
+            )))
 
 
 if __name__ == "__main__":
