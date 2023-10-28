725e821ea29e2553bca5c521f548f870fe818977
MahmoudEasa <MahmoudEasa@gmail.com>
2023-10-28 19:30:33 +0000

    Fix Task 3 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ Import app_views from api.v1.views """
 from api.v1.views import app_views
+from flask import jsonify
 
 
 @app_views.route("/status")
 def status():
     """ Returns a JSON: 'status': 'OK' """
-    return ({"Status": "OK"})
+    return jsonify({"Status": "OK"})
