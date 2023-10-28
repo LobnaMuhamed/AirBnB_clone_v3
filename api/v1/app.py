9ae8d970946a7c887a139c60e38857179e198195
MahmoudEasa <MahmoudEasa@gmail.com>
2023-10-28 19:19:26 +0000

    Solve Task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
-"""
-"""
+""" Start your API """
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
 
 
+app = Flask(__name__)
+app.register_blueprint(app_views)
+host = getenv('HBNB_API_HOST', '0.0.0.0')
+port = getenv('HBNB_API_PORT', 5000)
 
+
+@app.teardown_appcontext
+def teardown(err):
+    """Call storage.close
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host=host, port=port, threaded=True)
