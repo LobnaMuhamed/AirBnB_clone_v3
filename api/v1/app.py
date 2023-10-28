6243a08fd1608cb7fe9c1c94f7c549c3a4c5ebaf
MahmoudEasa <MahmoudEasa@gmail.com>
2023-10-28 19:43:10 +0000

    Fix Task 3 7

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,13 +3,10 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-host = getenv('HBNB_API_HOST', '0.0.0.0')
-port = int(getenv('HBNB_API_PORT', '5000'))
 
 
 @app.teardown_appcontext
@@ -19,4 +16,7 @@ def teardown(err):
 
 
 if __name__ == "__main__":
+    from os import getenv
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', '5000'))
     app.run(host=host, port=port, threaded=True)
