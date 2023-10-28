3d8d231780b933c18ed2c02c95be9b853e452733
MahmoudEasa <MahmoudEasa@gmail.com>
2023-10-28 20:12:27 +0000

    api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Start your API """
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 
@@ -10,9 +10,15 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def downtear(err):
+def teardown_appcontext(err):
     """ Call storage.close """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(err):
+    """ Not Found Page """
+    return (make_response(jsonify({"error": "Not found"}), 404))
 
 
 if __name__ == "__main__":
